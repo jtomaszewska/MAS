d8da07129254caadc45eda075ef83688b1a5f382
justyna <justyna.anna.tomaszewska@gmail.com>
2019-11-17 13:58:53 +0100

    test pull request 2

--- MP3/src/Admin.java
+++ MP3/src/Admin.java
@@ -1,5 +1,5 @@
 public class Admin extends User{
-    //test feature branch
+    //test feature branch better version
     public Admin(String firstName, String lastName, String email) {
         super(firstName, lastName, email);
     }
